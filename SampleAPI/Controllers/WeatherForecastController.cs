4692efcb766e6592704656b4dc3283b0780b4e0e
Amol Jagannath Dode <amdode@microsoft.com>
2020-05-22 15:03:34 -0700

    changes into array of input values

--- SampleAPI/Controllers/WeatherForecastController.cs
+++ SampleAPI/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace SampleAPI.Controllers
 	{
 		private static readonly string[] Summaries = new[]
 		{
-			"Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+			"Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Seattle"
 		};
 
 		private readonly ILogger<WeatherForecastController> _logger;
